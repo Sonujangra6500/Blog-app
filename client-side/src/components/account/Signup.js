e1db9c3fafee40bf296ffb809eb42d71fc52d380
Sonujangra6500 <164638260+Sonujangra6500@users.noreply.github.com>
2024-12-27 19:19:32 +0530

    Update Signup.js

--- client-side/src/components/account/Signup.js
+++ client-side/src/components/account/Signup.js
@@ -19,7 +19,7 @@ const Signup = () => {
     const UserRegister = async (e) => {
         try {
             e.preventDefault();
-            const res = await axios.post("http://localhost:3030/user/signup",
+            const res = await axios.post("https://blog-app-o5hc.onrender.com/user/signup",
                 user
             )
             if (res.data.message) {
